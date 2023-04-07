e88730908018b9da0cedf949f839a605866c0a55
Bash-Abdul <bashirtaiwo59@gmail.com>
2023-04-07 23:59:38 +0100

    easy bank

---
+++ public/script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('#menu');
+let nav = document.querySelector('#nav');
+
+menu.addEventListener('click', () =>{
+    if (nav.classList.contains('hidden')){
+        nav.classList.remove('hidden')
+    }
+    else{
+        nav.classList.add('hidden')
+    }
+})
