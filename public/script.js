2484369e3646b4bee997dc0fbd703b236d4bb2fa
Bash-Abdul <bashirtaiwo59@gmail.com>
2023-04-10 13:03:02 +0100

    easy bank

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+// let loader = document.querySelector('#loader');
+
+// window.addEventListener('load', function(){
+//     loader.style.display = "none"
+// })
+
 let menu = document.querySelector('#menu');
 let nav = document.querySelector('#nav');
 
